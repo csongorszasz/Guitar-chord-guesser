4d6e128ad47b631c80e65d56d3a20376cbc597a5
csongorszasz <szasz.csongor1@gmail.com>
2023-12-29 00:33:07 +0200

    Create empty UI classes

---
+++ hszpm-projekt/src/ui/ConfigPanel.java
@@ -0,0 +1,13 @@
+package ui;
+
+import javax.swing.*;
+
+public class ConfigPanel extends JPanel {
+//    private ChordSelectorPanel chordSelectorPanel;
+//    private SelectedChordPanel selectedChordPanel;
+//    private SongPropertiesPanel songPropertiesPanel;
+//
+//    public ConfigPanel() {
+//
+//    }
+}
