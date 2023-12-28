0e4020558cc9128d77cbef8894690208df99c0d3
csongorszasz <szasz.csongor1@gmail.com>
2023-12-29 00:28:05 +0200

    Set up frame

---
+++ hszpm-projekt/src/ui/MainFrame.java
@@ -0,0 +1,16 @@
+package ui;
+
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+    
+
+    public MainFrame() {
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setExtendedState(MAXIMIZED_BOTH);
+
+
+
+        setVisible(true);
+    }
+}
